451b484a13f184982404629606e2e03619690a23
ziobroaatwit <ziobroa@wit.edu>
2023-05-25 01:02:34 -0400

    Premade movement for class

---
+++ My project/Assets/Scripts/CameraUpdate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+public class CameraUpdate : MonoBehaviour
+{
+    [SerializeField] private Transform cameraPosition; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
