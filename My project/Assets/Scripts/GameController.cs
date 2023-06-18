d229a33bab9006cf5e9fe2a3558be0762532ea02
ziobroaatwit <ziobroa@wit.edu>
2023-06-17 23:39:44 -0400

    Lab1B FSM

--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -10,6 +10,8 @@ public class GameController: MonoBehaviour
     private int playerScore = 0;
     [SerializeField] TMP_Text playerScoreText;
     [SerializeField] TMP_Text npcScoreText;
+    [SerializeField] TMP_Text AgentOneState;
+    [SerializeField] TMP_Text AgentTwoState;
     public void Awake()
     {
         npcScoreText.text = "NPC Score: " + npcScore;
@@ -29,6 +31,54 @@ public class GameController: MonoBehaviour
         //Above is player score
         npcScoreText.text = "NPC Score: " + npcScore;
         playerScoreText.text = "Player Score: " + playerScore;
+    }
+    public void updateState(int type,int Agent)
+    {
+        //NPC Score
+        if (type == 0)
+        {
+            if(Agent==1)
+            {
+                AgentOneState.text = "AI 1 State: Patrol";
+            }
+            else if(Agent==2)
+            {
+                AgentTwoState.text = "AI 2 State: Patrol";
+            }
+        }
+        else if (type == 1)
+        {
+            if (Agent == 1)
+            {
+                AgentOneState.text = "AI 1 State: Attack";
+            }
+            else if (Agent == 2)
+            {
+                AgentTwoState.text = "AI 2 State: Chase Player";
+            }
+        }
+        else if (type == 2)
+        {
+            if (Agent == 1)
+            {
+                AgentOneState.text = "AI 1 State: Activated Flee";
+            }
+            else if (Agent == 2)
+            {
+                AgentTwoState.text = "AI 2 State: Flee";
+            }
+        }
+        else if (type == 3)
+        {
+            if (Agent == 1)
+            {
+                AgentOneState.text = "AI 1 State: Chase";
+            }
+            else if (Agent == 2)
+            {
+                AgentTwoState.text = "AI 2 State: Chase Target";
+            }
+        }
     }
 
 
--- My project/Assets/Scripts/NPCController.cs
+++ My project/Assets/Scripts/NPCController.cs
@@ -14,6 +14,9 @@ public class NPCController : MonoBehaviour
     private int waypointIndex=0;
     [SerializeField] GameController controller;
     [SerializeField] NavMeshAgent agent;
+    [SerializeField] int AgentID;
+    [SerializeField] Transform AgentOneTransform;
+    [SerializeField] Transform AgentTwoTransform;
 
     private Transform currentTarget;
 
@@ -22,13 +25,18 @@ public class NPCController : MonoBehaviour
     public NPCState currentState = NPCState.Patrol;
     private float distanceToPlayer;
     private float distanceToWaypoint;
+    private float distanceToAgent1;
+    private float distanceToAgent2;
     public float attackDistance = 10f;
     public float retreatDistance = 2.5f;
     public enum NPCState
     {
         Patrol,
         Attack,
-        Retreat
+        Retreat,
+        ChasePlayer,
+        ChaseTarget,
+        Chase
     }
     void Start()
     {
@@ -47,6 +55,9 @@ public class NPCController : MonoBehaviour
     {
         distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
         distanceToWaypoint = Vector3.Distance(currentTarget.transform.position,transform.position);
+        distanceToAgent1 = Vector3.Distance(AgentOneTransform.position, transform.position);
+        distanceToAgent2 = Vector3.Distance(AgentTwoTransform.position, transform.position);
+
         stateMachine();
         if(distanceToWaypoint<=5f)
         {
@@ -66,49 +77,163 @@ public class NPCController : MonoBehaviour
     }
     void stateMachine()
     {
-        switch (currentState)
+        if(this.AgentID==1)
         {
-            case NPCState.Patrol:
-                // Code to move the NPC in a patrol pattern here
-                agent.isStopped=false;
-                agent.SetDestination(currentTarget.position);
-                if (distanceToPlayer <= attackDistance)
-                {
-                    
-                    currentState = NPCState.Attack;
-                    Debug.Log("Attack State");
-                }
-                break;
+            switch (currentState)
+            {
+                case NPCState.Patrol:
+                    // Code to move the NPC in a patrol pattern here
+                    controller.updateState(0, AgentID);
+                    agent.isStopped = false;
+                    agent.SetDestination(currentTarget.position);
+                    //if (distanceToPlayer <= attackDistance)
+                    //{
 
-            case NPCState.Attack:
-                // Code to make the NPC attack the player here
-                agent.isStopped=true;
-                direction = (playerTransform.position - transform.position).normalized;
-                transform.position += direction * speed * Time.deltaTime;
+                       // currentState = NPCState.Attack;
+                       // Debug.Log("Attack State");
+                    //}
+                    if(distanceToWaypoint<30f && distanceToPlayer<20f)
+                    {
+                        currentState = NPCState.Retreat;
+                    }
+                    if (distanceToWaypoint >= 30f || distanceToPlayer>=20f)
+                    {
+                        currentState = NPCState.Chase;
+                    }
+                    break;
 
-                if (distanceToPlayer > attackDistance)
-                {
-                    currentState = NPCState.Patrol;
-                    Debug.Log("Patrol State");
-                }
-                else if (distanceToPlayer <= retreatDistance)
-                {
-                    currentState = NPCState.Retreat;
-                    Debug.Log("Retreat State");
-                }
-                break;
+                case NPCState.Attack:
+                    // Code to make the NPC attack the player here
+                    controller.updateState(1, AgentID);
+                    agent.isStopped = true;
+                    direction = (playerTransform.position - transform.position).normalized;
+                    transform.position += direction * speed * Time.deltaTime;
 
-            case NPCState.Retreat:
-                // Code to make the NPC move away from the player here
-                agent.isStopped=true;
-                direction = (playerTransform.position - transform.position).normalized;
-                transform.position += -direction * speed * Time.deltaTime;
-                if (distanceToPlayer > retreatDistance)
-                {
-                    currentState = NPCState.Patrol;
-                    Debug.Log("Patrol State");
-                }
-                break;
+                    if (distanceToPlayer > attackDistance && distanceToWaypoint<=10f)
+                    {
+                        currentState = NPCState.Patrol;
+                        Debug.Log("Patrol State");
+                    }
+                    else if (distanceToPlayer <= retreatDistance)
+                    {
+                        currentState = NPCState.Retreat;
+                        Debug.Log("Retreat State");
+                    }
+                    else if(distanceToPlayer > attackDistance && distanceToWaypoint > 10f)
+                    {
+                        currentState = NPCState.Chase;
+                        Debug.Log("Chase State");
+                    }
+                    break;
+                case NPCState.Retreat:
+                    controller.updateState(2, AgentID);
+                    // Code to make the NPC move away from the player here
+                    agent.isStopped = false;
+                    if(distanceToPlayer<=retreatDistance)
+                    {
+                        agent.isStopped = true;
+                        direction = (playerTransform.position - transform.position).normalized;
+                        transform.position += -direction * speed * Time.deltaTime;
+                    }
+                    if (distanceToWaypoint >= 30f || distanceToPlayer >= 20f)
+                    {
+                        currentState = NPCState.Chase;
+                    }
+                    break;
+                 case NPCState.Chase:
+                    controller.updateState(3, AgentID);
+                    agent.isStopped = true;
+                    direction = (playerTransform.position - transform.position).normalized;
+                    transform.position += direction * speed * Time.deltaTime;
+                    if (distanceToWaypoint < 30f && distanceToPlayer < 20f)
+                    {
+                        currentState = NPCState.Patrol;
+                    }
+                    break;
+
+            }
         }
+        else if(this.AgentID==2)
+        {
+            switch (currentState)
+            {
+                case NPCState.Patrol:
+                    // Code to move the NPC in a patrol pattern here
+                    controller.updateState(0, AgentID);
+                    agent.isStopped = true;
+                    agent.SetDestination(currentTarget.position);
+                    if (distanceToPlayer < distanceToWaypoint && distanceToAgent1>=15f)
+                    {
+
+                        currentState = NPCState.ChasePlayer;
+                        Debug.Log("ChasePlayer State");
+                    }
+                    if(distanceToPlayer>=distanceToWaypoint && distanceToAgent1>=15f)
+                    {
+                        currentState = NPCState.ChaseTarget;
+                        Debug.Log("ChaseTarget State");
+                    }
+                    if(distanceToAgent1<15f)
+                    {
+                        currentState = NPCState.Retreat;
+                    }
+                    break;
+                case NPCState.ChasePlayer:
+                    controller.updateState(1, AgentID);
+                    agent.isStopped = true;
+                    agent.SetDestination(currentTarget.position);
+                    direction = (playerTransform.position - transform.position).normalized;
+                    transform.position += direction * speed * Time.deltaTime;
+                    if (distanceToPlayer >= distanceToWaypoint)
+                    {
+
+                        currentState = NPCState.Patrol;
+                        Debug.Log("Patrol State");
+                    }
+                    if (distanceToAgent1<15f)
+                    {
+
+                        currentState = NPCState.Retreat;
+                        Debug.Log("Retreat State");
+                    }
+                    break;
+                case NPCState.ChaseTarget:
+                    controller.updateState(3, AgentID);
+                    agent.isStopped = false;
+                    agent.SetDestination(currentTarget.position);
+                    if (distanceToWaypoint<=2f)
+                    {
+                        currentState = NPCState.Patrol;
+                        Debug.Log("Patrol State");
+                    }
+                    if (distanceToPlayer <= distanceToWaypoint && distanceToAgent1 >= 15f)
+                    {
+
+                        currentState = NPCState.ChasePlayer;
+                        Debug.Log("ChasePlayer State");
+                    }
+                    if (distanceToAgent1 < 15f)
+                    {
+
+                        currentState = NPCState.Retreat;
+                        Debug.Log("Retreat State");
+                    }
+                    break;
+                case NPCState.Retreat:
+                    controller.updateState(2, AgentID);
+                    // Code to make the NPC move away from the player here
+                    agent.isStopped = true;
+                    direction = (AgentOneTransform.position - transform.position).normalized;
+                    transform.position += -direction * speed * Time.deltaTime;
+                    if (distanceToAgent1 >= 15f)
+                    {
+                        currentState = NPCState.Patrol;
+                        Debug.Log("Patrol State");
+                    }
+                    break;
+
+            }
+        }
+
     }
 }
